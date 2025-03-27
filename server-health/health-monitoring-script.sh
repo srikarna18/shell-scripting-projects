6918550654e35b09dd1efe76bddb0bdd0638c84d
srikarna18 <141703505+srikarna18@users.noreply.github.com>
2025-03-27 12:47:45 +0530

    Update health-monitoring-script.sh

--- server-health/health-monitoring-script.sh
+++ server-health/health-monitoring-script.sh
@@ -1,1 +1,28 @@
+#!/usr/bin/bash 
 
+#############  METADATA   ########
+#Author:
+#Date:
+#Version:
+#Description:
+##################################
+
+set -e #Exit on Error
+set -x #Debug Mode
+set -o #pipefail or typo
+or
+set -exo # Enable exit on error and debugging and typo
+
+
+
+df -h
+
+free -g
+
+nproc
+
+top
+
+ps -ef | grep -E "ERROR"  # finds both error logs
+
+ps -ef | grep ERROR | awk -F" " '{print $2, $3}' # prints pid ppid of error log
